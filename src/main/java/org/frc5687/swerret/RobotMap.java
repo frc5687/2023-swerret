34886cd349987add1dde71a0a77c8940137c6235
jackellingwood <jack.ellingwood@baxter-academy.org>
2023-09-29 17:37:15 -0400

    updated can and encoder IDs, but still doesn't drive well. (electrical pls help)

--- src/main/java/org/frc5687/swerret/RobotMap.java
+++ src/main/java/org/frc5687/swerret/RobotMap.java
@@ -15,14 +15,14 @@ public class RobotMap {
         public static class CAN {
 
                 public static class TALONFX {
-                        public static final int NORTH_WEST_OUTER = 8;
-                        public static final int NORTH_WEST_INNER = 7;
-                        public static final int NORTH_EAST_INNER = 2;
-                        public static final int NORTH_EAST_OUTER = 1;
-                        public static final int SOUTH_EAST_OUTER = 4;
-                        public static final int SOUTH_EAST_INNER = 3;
-                        public static final int SOUTH_WEST_INNER = 5;
-                        public static final int SOUTH_WEST_OUTER = 6;
+                        public static final int NORTH_WEST_OUTER = 1;
+                        public static final int NORTH_WEST_INNER = 2;
+                        public static final int NORTH_EAST_INNER = 8;
+                        public static final int NORTH_EAST_OUTER = 7;
+                        public static final int SOUTH_EAST_OUTER = 6;
+                        public static final int SOUTH_EAST_INNER = 5;
+                        public static final int SOUTH_WEST_INNER = 4;
+                        public static final int SOUTH_WEST_OUTER = 3;
                         public static final int TURRET = 9;
                         public static final int SHOOTER_ARM = 10;
                         public static final int SHOOTER_ROLLER = 11;
@@ -82,14 +82,13 @@ public class RobotMap {
          * DIO only one device can connect to each port, so the numbers should be
          * unique.
          */
-        public static class DIO {
-                public static final int ENCODER_NE = 10; // takes up 3 slots ABS, A, B channels.
-                public static final int ENCODER_NW = 0; // takes up 3 slots ABS, A, B channels.
-                public static final int ENCODER_SE = 6; // takes up 3 slots ABS, A, B channels.
-                public static final int ENCODER_SW = 3; // takes up 3 slots ABS, A, B channels.
+        public static class DIO { // roborio has ports 0-9, navx has ports 10-19 (navx 0-9 is 10-19).
+                public static final int ENCODER_NE = 7; // takes up 3 slots ABS, A, B channels.
+                public static final int ENCODER_NW = 4; // takes up 3 slots ABS, A, B channels.
+                public static final int ENCODER_SE = 10; // takes up 3 slots ABS, A, B channels.
+                public static final int ENCODER_SW = 1; // takes up 3 slots ABS, A, B channels.
 
-                public static final int TURRET_HALL = 9;
-                public static final int INTAKE_PROXIMITY_SENSOR = 13
-                ;
+                public static final int TURRET_HALL = 0;
+                // public static final int INTAKE_PROXIMITY_SENSOR = 13;
         }
 }
